cbef04d61fe492a90d4b291d03833bc38ed5ffeb
gabrik <gabriele.baldoni@gmail.com>
2022-07-28 16:39:49 +0200

    Adding missing copyright headers
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- src/nodes/sinks.rs
+++ src/nodes/sinks.rs
@@ -1,5 +1,18 @@
-use crate::{get_epoch_us, Latency};
+//
+// Copyright (c) 2022 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
 
+use crate::{get_epoch_us, Latency};
 use async_trait::async_trait;
 use futures::future::{AbortHandle, Abortable};
 use rand::Rng;
--- src/runtime.rs
+++ src/runtime.rs
@@ -1,5 +1,18 @@
-use std::convert::TryFrom;
+//
+// Copyright (c) 2022 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
 
+use std::convert::TryFrom;
 use std::fs::read_to_string;
 use zenoh_flow::async_std::sync::Arc;
 use zenoh_flow::runtime::dataflow::loader::{Loader, LoaderConfig};
